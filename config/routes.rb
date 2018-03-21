7d185eabd2f3c8d9441c1426ac1ddaf29a8aab86
moshimoshinekoko <mizuhotadokoro1@gmail.com>
2018-03-21 17:08:52 +0900

    neko

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  root 'pages#index'
+  get 'pages/help'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  post '/favorites', to: 'favorites#create'
+
+  resources 'users'
+  resources 'topics'
+
+  get '/favorites', to: 'favorites#index'
+end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
