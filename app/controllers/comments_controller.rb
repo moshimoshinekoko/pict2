6d0e108d99b7c6d3e770d2c58dbab970ed6f2843
moshimoshinekoko <mizuhotadokoro1@gmail.com>
2018-03-25 19:38:52 +0900

    comment save done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,14 +6,16 @@ class CommentsController < ApplicationController
 
   def new
     @comment = Comment.new
+    @comment.topic_id = "topicid"
   end
 
   def create
-    @comment = Comment.new(body: params[:body], topic_id: params[:topic_id], user_id: params[:user_id])
-    @comment.user_id = current_user.id
-    @comment.topic_id = params[:topic_id]
+    @comment = Comment.new(body: params[:comment][:body], topic_id: params[:comment][:topicid], user_id: params[:user_id])
+    @comment.user = current_user
+    @comment.topic_id = params[:comment][:topicid]
 
    if @comment.save
+
      redirect_to topics_path, notice: 'Comment Done'
    else
      redirect_to topics_path, notice: 'Comment Nope'
